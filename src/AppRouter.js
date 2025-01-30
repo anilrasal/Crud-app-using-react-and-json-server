1980a3d787c26afd1122e5b2c301355354323288
anil <annurasal123@gmail.com>
2025-01-31 00:19:18 +0530

    added error file

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -3,6 +3,7 @@ import App from "./App";
 import Add from "./Add";
 import Header from "./Header";
 import Edit from "./Edit";
+import Error from "./Error";
 
 const AppRouter = () => {
   return (
@@ -12,6 +13,7 @@ const AppRouter = () => {
         <Route path="/" element={<App />}></Route>
         <Route path="/create" element={<Add />}></Route>
         <Route path="/update/:id" element={<Edit />}></Route>
+        <Route path="/*" element={<Error />}></Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/Error.js
@@ -0,0 +1,13 @@
+import { useNavigate } from "react-router-dom";
+
+const Error = () => {
+  const navigate = useNavigate();
+  return (
+    <div>
+      <h2>page not found </h2>
+      <button onClick={() => Navigate("/")}>Home page</button>
+    </div>
+  );
+};
+
+export default Error;
